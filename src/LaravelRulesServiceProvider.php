ac7a1048b9a3977c7b4d7b411b20703c8d484bf6
Tina Hammar <tinahammar@gmail.com>
2022-06-23 14:19:55 +0200

    Update LaravelRulesServiceProvider.php

--- src/LaravelRulesServiceProvider.php
+++ src/LaravelRulesServiceProvider.php
@@ -5,7 +5,7 @@ namespace TantHammar\LaravelRules;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class LaravelRulesServiceProvider
+class LaravelRulesServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
